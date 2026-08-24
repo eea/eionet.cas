631c83b7265f62f5ff180c2543de5029ca5ba207
js <js@eworx.gr>
2026-08-24 12:56:45 +0300

    Refs #305779 drop-down menus removal

--- src/main/resources/static/pageops.js
+++ src/main/resources/static/pageops.js
@@ -8,7 +8,7 @@ function createCookie(name,value,days) {
         expires = "";
     }
     document.cookie = name+"="+escape(value)+expires+"; path=/;";
-};
+}
 
 function readCookie(name) {
     var nameEQ = name + "=";
@@ -23,14 +23,14 @@ function readCookie(name) {
         }
     }
     return null;
-};
+}
 
 
 /* ----- nodeutilities.js ----- */
 
 function hasClassName(node, class_name) {
     return new RegExp('\\b'+class_name+'\\b').test(node.className);
-};
+}
 
 function addClassName(node, class_name) {
     if (!node.className) {
@@ -40,7 +40,7 @@ function addClassName(node, class_name) {
         // cleanup
         node.className = className.split(/\s+/).join(' ');
     }
-};
+}
 
 function removeClassName(node, class_name) {
     var className = node.className;
@@ -51,22 +51,7 @@ function removeClassName(node, class_name) {
         className = className.replace(/\s+/g, ' ');
         node.className = className.replace(/\s+$/g, '');
     }
-};
-
-function replaceClassName(node, old_class, new_class, ignore_missing) {
-    if (ignore_missing && !hasClassName(node, old_class)) {
-        addClassName(node, new_class);
-    } else {
-        var className = node.className;
-        if (className) {
-            // replace
-            className = className.replace(new RegExp('\\b'+old_class+'\\b'), new_class);
-            // cleanup
-            className = className.replace(/\s+/g, ' ');
-            node.className = className.replace(/\s+$/g, '');
-        }
-    }
-};
+}
 
 /* ----- fullscreenmode.js ----- */
 function toggleFullScreenMode() {
@@ -80,30 +65,13 @@ function toggleFullScreenMode() {
         addClassName(body, 'fullscreen');
         createCookie('fullscreenMode', '1');
     }
-};
+}
 
 function fullscreenModeLoad() {
     // based on cookie
     if (readCookie('fullscreenMode') == '1') {
         var body = document.body;
         addClassName(body, 'fullscreen');
-    }
-};
-
-function startList() {
-    if (document.all && document.getElementById) {
-        navRoot = document.getElementById("dropdowns");
-        for (i=0; i<navRoot.childNodes.length; i++) {
-            node = navRoot.childNodes[i];
-            if (node.nodeName=="LI") {
-                node.onmouseover=function() {
-                    addClassName(this, 'over');
-                }
-                node.onmouseout=function() {
-                    removeClassName(this, 'over');
-                }
-            }
-        }
     }
 }
 
@@ -120,6 +88,5 @@ function addEvent(obj, evType, fn){
  }
 }
 
-addEvent(window,'load',startList);
 addEvent(window,'load',fullscreenModeLoad);
 
